99a4674da24f14d3d26aa2f289e62da38d66ad5d
Ryan Fleck <ryan.fleck@protonmail.com>
2018-12-29 11:51:48 -0700

    Enforce Python 2, add input safety for hosts path.

--- SiteBlocker.py
+++ SiteBlocker.py
@@ -1,16 +1,25 @@
+#!/usr/bin/env python
+
+"""Makes a simple modification to the user Hosts file."""
+
+__author__ = "Saimoon Azad"
+__copyright__ = "Copyright 2018 Saimoon Azad"
+__credits__ = ["Saimoon Azad","Ryan Fleck"]
+
+import re
 
 host_address = raw_input("Enter the full path of the host file: ")
 ip_address = r"127.0.0.1"
-host_address = host_address + "\hosts"
-
+if( not re.match("(.*)\\hosts$", host_address) ):
+    host_address = host_address + "\hosts"
 
 choice = raw_input("Would you like to block a website, or remove a site from the blocked list? \nEnter B for blocking, or R for removing ")
 blockList = []
 
-if choice == 'B':
+if choice.upper() == 'B':
     while(True):
         site_name= raw_input("Enter name of the site or sites you want to block, Press D when done: ")
-        if(site_name == 'D'):
+        if(site_name.upper() == 'D'):
             break
         blockList.append(site_name)
     for l in blockList:
@@ -18,13 +27,13 @@ if choice == 'B':
             f.write("\n" + ip_address + " " + l)
             f.close()
 
-if choice == 'R':
+if choice.upper() == 'R':
     f = open(host_address,"r")
     lines = f.readlines()
     f.close()
     while(True):
         site_name= raw_input("Enter name of the site or sites you want to unblock, Press D when done: ")
-        if(site_name == 'D'):
+        if(site_name.upper() == 'D'):
             break
         blockList.append(site_name)
     f = open(host_address,"w")
